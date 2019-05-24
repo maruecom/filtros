fff67a00148301bbfc2c4f0f28ef83bcc748ee12
marucomerci <marianacomerci@oceano.com.ar>
2019-05-23 21:19:36 -0300

    m

--- javascript.js
+++ javascript.js
@@ -2,11 +2,11 @@
 // Tenemos un listado de productos
 
 var productos = [
-    {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-    {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-    {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-    {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-    {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+    {nombre: "zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+    {nombre: "zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+    {nombre: "bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+    {nombre: "bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+    {nombre: "zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
   ]
   
   
@@ -56,13 +56,55 @@ var productos = [
       listado.removeChild(listado.firstChild);
   }
   
-  // despues les toca a uds el resto :)
+  let busqueda = document.querySelector("input");
+ 
   
   
   
+
+  for (let i = 0; i < productos.length; i++){
+    
+
+    if (busqueda.value == productos[i].tipo  || busqueda.value == productos[i].color || busqueda.value == productos[i].nombre) {
+     
+      
+      var div = document.createElement("div")
+      div.classList.add("producto")
+    
+      var titulo = document.createElement("p")
+      titulo.classList.add("titulo")
+      titulo.textContent = productos[i].nombre
+      
+      var imagen = document.createElement("img");
+      imagen.setAttribute('src', productos[i].img);
+    
+      div.appendChild(titulo)
+      div.appendChild(imagen)
+    
+      // Agregamos el div al listado 
+      listado.appendChild(div)
+      
+    }
+
   }
+   
+
+}
+  
+
+const nombre = "Ada";
+
+const profesion = "programadora";
+
+console.log(`mi nombre es ${nombre} y soy ${profesion}`); 
+
+// completá el código acá
   
   
+  
+
+
+
   // Tarea: 
   // Si el usuario escribe "zapato", mostrar los zapatos
   // Si el usuario escribe "bota", mostrar las botas
